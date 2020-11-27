e6223fd7ff54380aa5b57b7ceacfa39e3a9a3779
KonTroAl <troshenkin.k@yandex.ru>
2020-11-27 21:22:21 +0500

    task_2 in work

--- task_2.js
+++ task_2.js
@@ -3,3 +3,35 @@
 // Задачи:
 // Организовать такой массив для хранения товаров в корзине;
 // Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
+
+'use strict'
+
+
+// let a = { name: 'PS', cost: 47000 }
+// let b = { name: 'Xbox', cost: 46000 }
+
+let basket = [{ name: 'PS', cost: 47000 }, { name: 'Xbox', cost: 46000 }];
+
+function countBasketPrice(basket) {
+    const sum = []
+    let a = 0
+    let res = 0
+    for (let i = 0; i < basket.length; i++) {
+        for (let key in basket[i]) {
+            // document.writeln(key)
+            if (key === "cost") {
+                sum.push(basket[i][key]);
+            }
+        }
+    }
+    for (let i = 0; i < sum.length; i++) {
+        a = sum[i]
+        res = res + a
+    }
+
+    document.writeln(res);
+}
+
+countBasketPrice(basket)
+
+// alert(basket[0]["name"])
